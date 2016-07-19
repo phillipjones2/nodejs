46098b53c41f504a5036a42f9f8b565414905c2b
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-19 18:53:00 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+var port = 3000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req,res){
+  res.send("Hello World");
+});
+
+app.listen(port, function(err){
+  console.log('running server on port' + port);
+});
